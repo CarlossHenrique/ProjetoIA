5898b68fe8471c29888c683a5a84c6e41c1e1088
carloshenri62@gmail.com <carloshenri62@gmail.com>
2018-05-30 10:03:47 -0300

    second commit

--- src/br/com/ia/geneticalgorithms/main/Principal.java
+++ src/br/com/ia/geneticalgorithms/main/Principal.java
@@ -4,13 +4,15 @@ import br.com.ia.geneticalgorithms.algorithms.Geneticalgorithms;
 
 public class Principal {
 
-	// não estou criar uma variavél para a cadeia de caracteres sem dar 
-	//um erro, por isso as cadeias tem que ser alteradas manualmente
 
 	public static void main(String[] args) {
 	
 	Scanner in = new Scanner(System.in);	
 
+	System.out.println("Digite a cadeia de caracteres:");
+	
+	String caract = in.nextLine();
+	
 	System.out.println("Taxa de croosover:");
 	
 	double taxaDeCrossover=in.nextDouble();
@@ -25,8 +27,7 @@ public class Principal {
 	
 	if(taxaDeCrossover>0&&taxaDeCrossover<=1&& taxaDeMutacao>0&&taxaDeMutacao<=1){
 	
-		// o primeiro parametro representa a cadeia de caracteres.
-		ga.executar("o pelo do pé de pedro é preto", taxaDeCrossover,taxaDeMutacao);
+		ga.executar(caract, taxaDeCrossover,taxaDeMutacao);
 	
 	
 	}
